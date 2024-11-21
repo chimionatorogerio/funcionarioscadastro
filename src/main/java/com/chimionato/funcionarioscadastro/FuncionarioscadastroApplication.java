7cd7bb2b20712f22db4c9238dc01700f2fc847e7
Rogério Chimionato <chimionato.rogerio@gmail.com>
2024-11-21 20:21:04 -0300

    FuncionarioscadastroApplication: incluí método de teste da api. Teste no Postman ok

--- src/main/java/com/chimionato/funcionarioscadastro/FuncionarioscadastroApplication.java
+++ src/main/java/com/chimionato/funcionarioscadastro/FuncionarioscadastroApplication.java
@@ -2,9 +2,18 @@ package com.chimionato.funcionarioscadastro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class FuncionarioscadastroApplication {
+	@GetMapping
+	public String getHomeTeste() {
+		return "API Home está funcionando";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(FuncionarioscadastroApplication.class, args);
