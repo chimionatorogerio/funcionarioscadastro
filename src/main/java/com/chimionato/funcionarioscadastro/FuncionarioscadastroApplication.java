a42880ada07720ecdccc671c01c5c17f5e5d4e20
Rogério Chimionato <chimionato.rogerio@gmail.com>
2024-11-21 19:00:14 -0300

    inicia projeto

---
+++ src/main/java/com/chimionato/funcionarioscadastro/FuncionarioscadastroApplication.java
@@ -0,0 +1,13 @@
+package com.chimionato.funcionarioscadastro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuncionarioscadastroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuncionarioscadastroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chimionato/funcionarioscadastro/FuncionarioscadastroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chimionato.funcionarioscadastro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuncionarioscadastroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
