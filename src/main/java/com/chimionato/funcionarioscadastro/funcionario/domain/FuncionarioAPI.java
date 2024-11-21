66e805244fce584a932a7e94d4cb683bac444ec5
Rogério Chimionato <chimionato.rogerio@gmail.com>
2024-11-21 20:49:05 -0300

    criei FuncionarioAPI

---
+++ src/main/java/com/chimionato/funcionarioscadastro/funcionario/domain/FuncionarioAPI.java
@@ -0,0 +1,13 @@
+package com.chimionato.funcionarioscadastro.funcionario.domain;
+
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/v1/funcionario")
+public interface FuncionarioAPI {
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    FuncionarioResponse postFuncionario(@Valid @RequestBody FuncionarioRequest funcionarioRequest);
+}
