d1e1f643193cf973dc780b069bab490fef1df9e5
Rogério Chimionato <chimionato.rogerio@gmail.com>
2024-11-22 17:27:22 -0300

    FuncionarioService: incluí método

--- src/main/java/com/chimionato/funcionarioscadastro/funcionario/application/service/FuncionarioService.java
+++ src/main/java/com/chimionato/funcionarioscadastro/funcionario/application/service/FuncionarioService.java
@@ -1,8 +1,12 @@
 package com.chimionato.funcionarioscadastro.funcionario.application.service;
 
+import com.chimionato.funcionarioscadastro.funcionario.application.api.FuncionarioListResponse;
 import com.chimionato.funcionarioscadastro.funcionario.application.api.FuncionarioRequest;
 import com.chimionato.funcionarioscadastro.funcionario.application.api.FuncionarioResponse;
 
+import java.util.List;
+
 public interface FuncionarioService {
     FuncionarioResponse postFuncionario(FuncionarioRequest funcionarioRequest);
+    List<FuncionarioListResponse> getTodosFuncionarios();
 }
