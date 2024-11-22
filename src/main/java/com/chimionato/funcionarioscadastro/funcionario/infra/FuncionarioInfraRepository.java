1e521a6a8051d04a63b83cba5b2ece69d226a3b2
Rogério Chimionato <chimionato.rogerio@gmail.com>
2024-11-22 16:38:10 -0300

    criei FuncionarioInfraRepository - teste ok no Postman só com logs e return - em seguida, incluí método e dependência

---
+++ src/main/java/com/chimionato/funcionarioscadastro/funcionario/infra/FuncionarioInfraRepository.java
@@ -0,0 +1,24 @@
+package com.chimionato.funcionarioscadastro.funcionario.infra;
+
+import com.chimionato.funcionarioscadastro.funcionario.application.repository.FuncionarioRepository;
+import com.chimionato.funcionarioscadastro.funcionario.domain.Funcionario;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Repository;
+import sun.security.krb5.internal.ccache.MemoryCredentialsCache;
+
+@Repository
+@Log4j2
+@RequiredArgsConstructor
+public class FuncionarioInfraRepository implements FuncionarioRepository {
+
+    private final ClienteSpringDataJPARepository clienteSpringDataJPARepository;
+
+    @Override
+    public Funcionario postFuncionario(Funcionario funcionario) {
+        log.info("[inicia]   - FuncionarioInfraRepository - postFuncionario");
+        clienteSpringDataJPARepository.save(funcionario);
+        log.info("[FINALIZA] - FuncionarioInfraRepository - postFuncionario");
+        return funcionario;
+    }
+}
