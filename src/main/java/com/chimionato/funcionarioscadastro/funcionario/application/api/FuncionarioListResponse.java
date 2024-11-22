b8318a9ec12f1759493246a5be5c9f3561b24524
Rogério Chimionato <chimionato.rogerio@gmail.com>
2024-11-22 18:15:44 -0300

    FuncionarioListResponse: criei construtor e método converte

--- src/main/java/com/chimionato/funcionarioscadastro/funcionario/application/api/FuncionarioListResponse.java
+++ src/main/java/com/chimionato/funcionarioscadastro/funcionario/application/api/FuncionarioListResponse.java
@@ -1,9 +1,14 @@
 package com.chimionato.funcionarioscadastro.funcionario.application.api;
 
+import com.chimionato.funcionarioscadastro.funcionario.domain.Funcionario;
 import jakarta.validation.constraints.NotBlank;
+import lombok.Value;
 
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
+@Value
 public class FuncionarioListResponse {
     private UUID idFuncionario;
     private String nomeCompleto;
@@ -11,4 +16,19 @@ public class FuncionarioListResponse {
     private String salario;
     private String telefone;
     private String endereco;
+
+    public static List<FuncionarioListResponse> converte(List<Funcionario> funcionarios) {
+        return funcionarios.stream()
+                .map(FuncionarioListResponse::new)
+                .collect(Collectors.toList());
+    }
+
+    private FuncionarioListResponse(Funcionario funcionario) {
+        this.idFuncionario = funcionario.getIdFuncionario();
+        this.nomeCompleto = funcionario.getNomeCompleto();
+        this.designacao = funcionario.getDesignacao();
+        this.salario = funcionario.getSalario();
+        this.telefone = funcionario.getTelefone();
+        this.endereco = funcionario.getEndereco();
+    }
 }
