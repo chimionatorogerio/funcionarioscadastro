38ffdd51a4aca7be5e7999181640d0a732fcb236
Rogério Chimionato <chimionato.rogerio@gmail.com>
2024-11-22 17:10:44 -0300

    FuncionarioController: teste ok no Postman só com logs - em seguida incluí método e retorno

--- src/main/java/com/chimionato/funcionarioscadastro/funcionario/application/api/FuncionarioAPI.java
+++ src/main/java/com/chimionato/funcionarioscadastro/funcionario/application/api/FuncionarioAPI.java
@@ -4,6 +4,8 @@ import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/v1/funcionario")
 public interface FuncionarioAPI {
--- src/main/java/com/chimionato/funcionarioscadastro/funcionario/application/api/FuncionarioController.java
+++ src/main/java/com/chimionato/funcionarioscadastro/funcionario/application/api/FuncionarioController.java
@@ -5,6 +5,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @Log4j2
 @RequiredArgsConstructor
@@ -18,5 +20,13 @@ public class FuncionarioController implements FuncionarioAPI {
         FuncionarioResponse funcionarioCriado = funcionarioService.postFuncionario(funcionarioRequest);
         log.info("[FINALIZA] FuncionarioController - postFuncionario");
         return funcionarioCriado;
+    }
+
+    @Override
+    public List<FuncionarioListResponse> getTodosFuncionarios() {
+        log.info("[inicia]     FuncionarioController - getTodosFuncionarios");
+        List<FuncionarioListResponse> funcionarios = funcionarioService.getTodosFuncionarios();
+        log.info("[FINALIZA]   FuncionarioController - getTodosFuncionarios");
+        return funcionarios;
     }
 }
