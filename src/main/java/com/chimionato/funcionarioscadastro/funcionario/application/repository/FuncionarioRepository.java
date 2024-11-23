f47f5dc173652211d9e83d862d131a73eb6dbf2f
Rogério Chimionato <chimionato.rogerio@gmail.com>
2024-11-23 19:39:01 -0300

    FuncionarioRepository: criei método

--- src/main/java/com/chimionato/funcionarioscadastro/funcionario/application/repository/FuncionarioRepository.java
+++ src/main/java/com/chimionato/funcionarioscadastro/funcionario/application/repository/FuncionarioRepository.java
@@ -3,8 +3,10 @@ package com.chimionato.funcionarioscadastro.funcionario.application.repository;
 import com.chimionato.funcionarioscadastro.funcionario.domain.Funcionario;
 
 import java.util.List;
+import java.util.UUID;
 
 public interface FuncionarioRepository {
     Funcionario postFuncionario(Funcionario funcionario);
     List<Funcionario> getTodosFuncionarios();
+    Funcionario getUmFuncionarioPeloId(UUID idFuncionario);
 }
